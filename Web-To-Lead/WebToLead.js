db97318f344e6fa2666429a62c46d43118b47e9a
anshulkumar1997 <anshulkumar1997@gmail.com>
2025-03-13 21:41:46 +0100

    reCAPTCHA by Google

--- Web-To-Lead/WebToLead.js
+++ Web-To-Lead/WebToLead.js
@@ -1,8 +1,32 @@
-function beforesubmit() {
-  let outputdate = document.querySelector(".outputdate");
-  let inputdate = document.querySelector(".inputdate");
-  console.log("inputdate", inputdate.value); // String ---> date (en_IN)
+function beforesubmit(event) {
+  if (captchachecked) {
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
+    console.log("inputdate", inputdate.value); // String ---> date (en_IN)
 
-  let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
-  outputdate.value = formattedDate;
+    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+    outputdate.value = formattedDate;
+  } else {
+    alert("Please check the reCAPTCHA box to submit the lead");
+    event.preventDefault();
+  }
+}
+
+let captchachecked = false;
+
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
+
+function captchasuccess() {
+  captchachecked = true;
 }
